ea7ac7388749caf9ae836deb44f054e6fa235dfa
Chris Gilbert <chrisagilbert.cg@gmail.com>
2025-12-25 20:32:10 -0500

    Final Die inheritor

--- src/character.rs
+++ src/character.rs
@@ -12,3 +12,18 @@ pub struct Character {
     armor: Option<Armor>,
     body: Body
 }
+
+impl Character {
+    /// Creates a default character with given weapon and armor.
+    pub fn new(weapon: Option<Weapon>,
+    armor: Option<Armor>,
+    body: Body)
+    -> Character {
+       Character { weapon, armor, body } 
+    }
+
+    /// Retrieves a `Character`'s name
+    pub fn get_name(&self) -> &str {
+        self.body.name.as_str()
+    }
+}
